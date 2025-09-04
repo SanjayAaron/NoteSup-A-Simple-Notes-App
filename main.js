0c9a9e8a877773a197567183f028df0709828004
Sanjay Aaron <sanjayarron046@gmail.com>
2025-09-04 15:06:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+// Selecting Popup Box, Overlay, Button
+var popupbox =document.querySelector(".popup-box");
+var popupoverlay=document.querySelector(".popup-overlay");
+var popupbutton=document.getElementById("popup-button");
+
+popupbutton.addEventListener("click",function(){
+   popupoverlay.style.display="block";
+   popupbox.style.display="block";
+});
+//Selecting Cancel Button
+var cancelbutton=document.getElementById("cancel-book")
+cancelbutton.addEventListener("click", function(){
+   event.preventDefault()
+   popupoverlay.style.display="none";
+   popupbox.style.display="none";
+})
+//Selecting Book Container,book-title-input,book-topic-input,book-note-input
+
+container=document.querySelector(".container")
+addbook=document.getElementById("add-book")
+booktitleinput=document.getElementById("book-title-input")
+booktitleinput=document.getElementById("book-topic-input")
+booknoteinput=document.getElementById("book-note-input")
+
+book.addEventListener("click", function(event){
+   event.preventDefault()
+   var div=document.createElement("div")
+   div.setAttribute("class","book-container")
+   div.innerHTML=`
+    <h2>${booktitleinput.value}</h2>
+    <h3>${booktopicinput.value}</h3>
+    <p>${booknoteinput.value}</p>
+    <button>Delete</button>`
+   container.append(div)
+   popupoverlay.style.display="none";
+   popupbox.style.display="none";
+})
